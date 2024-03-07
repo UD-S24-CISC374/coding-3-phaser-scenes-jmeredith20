bf741efab943ec44aa742098b6de036181a37338
Jacob Meredith <79116619+jmeredith20@users.noreply.github.com>
2024-03-07 15:07:19 -0500

    Added all images and backgrounds

--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -13,26 +13,25 @@ export default class PreloadScene extends Phaser.Scene {
         this.load.image("sky", "assets/sky.png");
         this.load.image("house", "assets/house.png");
         this.load.image("bone", "assets/bone.png");
-        this.load.image("ground", "assets/platform.png");
+        this.load.image("ground", "assets/dirt.png");
 
         //Scene 2 Requirements
         this.load.image("cabin", "assets/cabin.png");
         this.load.image("portal", "assets/portal.png");
         this.load.image("meat", "assets/meat.png");
         this.load.image("log", "assets/log.png");
-        /*
+
         //Scene 3 Requirements
         this.load.image("space", "assets/space.png");
-        this.load.image("portal", "assets/portal.png");
+        this.load.image("ufo", "assets/ufo.png");
         this.load.image("amongus", "assets/amongus.png");
         this.load.image("spaceground", "assets/spaceground.png");
 
         //Scene 4 Requirements
-        this.load.image("space", "assets/space.png");
-        this.load.image("portal", "assets/portal.png");
-        this.load.image("star", "assets/star.png");
-        this.load.image("ground", "assets/platform.png");
-*/
+        this.load.image("ship", "assets/ship.png");
+        this.load.image("restart", "assets/restart.png");
+        this.load.image("alien", "assets/alien.png");
+        this.load.image("pipe", "assets/pipe.png");
 
         this.load.spritesheet("dude", "assets/dude.png", {
             frameWidth: 32,
